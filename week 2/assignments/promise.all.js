79d65ccdaeac08c4476090f5b66459aa77d6234b
divyank0203 <divyankh444@gmail.com>
2025-08-15 15:00:36 +0530

    promise.all

--- week 2/assignments/promise.all.js
+++ week 2/assignments/promise.all.js
@@ -6,22 +6,39 @@
 
 function one(){
     const p1 = new Promise(function(resolve){
-        setTimeout(resolve, 1000);
+        setTimeout(function(){
+            console.log("Promise 1 resolved after 1 second");
+            resolve("Result from Promise 1");
+        }, 1000);
     })
     return p1;
 }
 function two(){
     const p2 = new Promise(function(resolve){
-        setTimeout(resolve, 2000);
+        setTimeout(function(){
+            console.log("Promise 2 resolved after 2 seconds")
+            resolve("Result from Promise 2")
+        }, 2000);
     })
-    return p1;
+    return p2;
 }
 function three(){
     const p3 = new Promise(function(resolve){
         setTimeout(function(){
+            console.log("Promise 3 resolved after 3 seconds")
+            resolve("Result from promise 3")
             
-        })
+        }, 3000)
     })
-    return p1;
+    return p3;
 }
 const ps = [one(), two(), three()];
+
+const start = new Date().getTime();
+return Promise.all(ps)
+    .then(function(results){
+        const end = new Date().getTime();
+        console.log("All promises resolved");
+        console.log(results);
+        console.log(`Time taken === ${end-start} ms`);
+    })
