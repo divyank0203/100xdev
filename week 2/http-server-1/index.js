32a4e73fa4b05fb8a3398b0dea0bae73e6477759
divyank0203 <divyankh444@gmail.com>
2025-08-16 17:00:09 +0530

    query input and response of it

--- week 2/http-server-1/index.js
+++ week 2/http-server-1/index.js
@@ -2,18 +2,30 @@
 
 const express = require('express')
 const app = express();
-const port = 3000
+const port = 3001
 
 //fs.redFileSync('index.html', 'utf8', (err, data) => {
 //  if (err) throw err;
 //  console.log(data);
 //});
 
+function calculatesum(a, b){
+    return a+b;
+}
 
 //GET is the default method for a request. '/' is the route.
 //app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', function(req, res){
+//   res.send('Hello World!')
+// })
+
 app.get('/', function(req, res){
-  res.send('Hello World!')
+    const a = req.query.a;  // (/?a=5&b=10) This is how you can pass query parameters
+    const b = req.query.b;
+    const ans = calculatesum((a), (b));
+    res.send(`Sum of ${a.toString()} and ${b.toString()} is ${ans.toString()}`);
+    // always send a string response
+    // res.send(`Sum of 5 and 10 is 15`);
 })
 
 
