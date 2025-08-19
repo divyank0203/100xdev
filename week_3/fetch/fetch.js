34dd64a625535a62c7f972bdbd50a1dc240363ea
divyank0203 <divyankh444@gmail.com>
2025-08-19 15:57:54 +0530

    fetch

---
+++ week_3/fetch/fetch.js
@@ -0,0 +1,19 @@
+        // function getData(){
+        //     fetch('https://fakerapi.it/api/v1/persons')
+        //     .then(function(response){
+        //         response.json() //this also returns a promise so one more then to handle the data
+        //         .then(function(data){
+        //             console.log(data);
+        //         })
+        //     })
+        // }
+
+    // The above code can also be written in asyn await fashion
+
+        async function getData(){
+            const response = await fetch('https://fakerapi.it/api/v1/persons');
+            const data = await response.json();
+            console.log(data);
+        }
+
+        
