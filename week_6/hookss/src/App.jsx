91356633b32721a5ea349b8e95d43c34f110d9d3
divyank0203 <divyankh444@gmail.com>
2025-09-05 17:51:29 +0530

    useEffect

--- week_6/hookss/src/App.jsx
+++ week_6/hookss/src/App.jsx
@@ -4,23 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [todos, setTodo] = useState([]);
-// every 10 seconds fetch the data from the server and update the state todos
-  useEffect(function(){
-    setInterval(() => {
-    fetch("https://sum-server.100xdevs.com/todos") //
-    .then(async function(res){
-      const data = await res.json();
-      setTodo(data);
-      console.log(data);
-    })      
-    }, 10000);
 
-  }, [])
+const [selectedId, setSelectedId] = useState(1);
+// this state will help change the id prop passed to the Todo component
+// when the id prop changes the useEffect in the Todo component will run again and fetch the data from the server again
 
   
   return (
     <>
+    <button onClick={function(){setSelectedId(1)}}>1</button>
+    <button onClick={function(){setSelectedId(2)}}>2</button>
+    <button onClick={function(){setSelectedId(3)}}>3</button>
+    <button onClick={function(){setSelectedId(4)}}>4</button>
     {todos.map(function(todo){
       return <Todo title={todo.title} description={todo.description} key={todo.id}></Todo>
     })}
@@ -34,6 +29,20 @@ export default App
 
 // a component to render the todos after fetching from the server and updating the state
 function Todo(props){
+
+    const [todos, setTodo] = useState([]);
+// every 10 seconds fetch the data from the server and update the state todos
+  useEffect(function(){
+    
+    fetch("https://sum-server.100xdevs.com/todos") //
+    .then(async function(res){
+      const data = await res.json();
+      setTodo(data);
+      console.log(data);
+    })      
+    
+
+  }, [props.id]) // run this effect only when the id prop changes
 
   return (
     <>
