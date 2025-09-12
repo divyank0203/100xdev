45e2f2c1499c849478c0f06183d2160aeaba6a09
divyank0203 <divyankh444@gmail.com>
2025-09-12 23:56:07 +0530

    jotai atoms render

--- week_7/jotai/src/App.jsx
+++ week_7/jotai/src/App.jsx
@@ -7,15 +7,18 @@ import { networkAtom, jobAtom, messageAtom, notificationAtom } from './Atoms'
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const [network, setNetwork] = useAtom(networkAtom);
+  const [job, setJob] = useAtom(jobAtom);
+  const [message, setMessage] = useAtom(messageAtom);
+  const [notification, setNotification] = useAtom(notificationAtom);
 
   return (
     <>
       <button style={{margin: "10px"}}>Home</button>
-      <button style={{margin: "10px"}}>Networks ()</button>
-      <button style={{margin: "10px"}}>Jobs ()</button>
-      <button style={{margin: "10px"}}>Messages ()</button>
-      <button style={{margin: "10px"}}>Notifications ()</button>
+      <button style={{margin: "10px"}}>Networks ({network>=100?"99+":network})</button>
+      <button style={{margin: "10px"}}>Jobs ({job>=100?"99+":job})</button>
+      <button style={{margin: "10px"}}>Messages ({message>=100?"99+":message})</button>
+      <button style={{margin: "10px"}}>Notifications ({notification>=100?"99+":notification})</button>
 
       <button style={{margin: "10px"}}>My Profile</button>
     </>
