21a5c407a2e3aed05f1dec9dbcf0bb05a36446b3
divyank0203 <divyankh444@gmail.com>
2025-09-12 23:42:39 +0530

    added a button

--- week_7/jotai/src/App.jsx
+++ week_7/jotai/src/App.jsx
@@ -5,13 +5,14 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  
+
 
   return (
     <>
-      <button style={{margin: "10px"}}>Home ()</button>
+      <button style={{margin: "10px"}}>Home</button>
       <button style={{margin: "10px"}}>Networks ()</button>
       <button style={{margin: "10px"}}>Jobs ()</button>
+      <button style={{margin: "10px"}}>Messages ()</button>
       <button style={{margin: "10px"}}>Notifications ()</button>
 
       <button style={{margin: "10px"}}>My Profile</button>
