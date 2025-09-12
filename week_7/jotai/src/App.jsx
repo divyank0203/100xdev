4cded1d9c74e21d5ad8405f2c52334d61d1c1618
divyank0203 <divyankh444@gmail.com>
2025-09-12 23:49:28 +0530

    npm install jotai

--- week_7/jotai/src/App.jsx
+++ week_7/jotai/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useAtom } from 'jotai';
+import { networkAtom, jobAtom, messageAtom, notificationAtom } from './Atoms'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
 
   return (
---
+++ week_7/jotai/src/Atoms.js
@@ -0,0 +1,6 @@
+import {atom, useAtom} from 'jotai'
+
+export const networkAtom = atom(104);
+export const jobAtom = atom(0);
+export const messageAtom = atom(69);
+export const notificationAtom = atom(173);
