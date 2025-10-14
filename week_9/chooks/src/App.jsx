f3390bafe690334159f8b511fe0006ec9f39fc15
divyank0203 <divyankh444@gmail.com>
2025-10-14 23:15:57 +0530

    useTodos

--- week_9/chooks/src/App.jsx
+++ week_9/chooks/src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function useTodos(n){
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function(){
+
+    setInterval(function(){
+    fetch('https://sum-server.100xdevs.com/todos')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+      setTodos(data.todos);
+      setLoading(false);
+    })
+    }, n*1000)
+
+  }, []);
+  return {todos, loading};
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
