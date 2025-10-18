647fcd1a991a8a63f920f42e70d94121f8d5a4c1
divyank0203 <divyankh444@gmail.com>
2025-10-18 22:38:40 +0530

    swr library for data fetching

--- week_9/chooks/src/App.jsx
+++ week_9/chooks/src/App.jsx
@@ -1,10 +1,23 @@
 import { useState, useEffect } from 'react'
+import useSWR from 'swr' // this is a data fetching library
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const fetcher = url => fetch(url).then(res => res.json()); // fetcher function for useSWR
 
+// custom hook to fetch todos every n seconds
 function useTodos(n){
+
+  // Using SWR for data fetching
+  // const { data, error, isLoading } = useSWR('https://sum-server.100xdevs.com/todos', fetcher);
+
+  // if (error) return <div>failed to load</div>
+  // if (isLoading) return <div>loading...</div>
+  // const todos1 = data.todos;
+  // return {todos1};
+
+  // Using useEffect and setInterval for periodic fetching
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -17,11 +30,11 @@ function useTodos(n){
     })
     .then(function(data){
       setTodos(data.todos);
-      setLoading(false);
+      setLoading(false); // data is fetched every n seconds 
     })
     }, n*1000)
 
-  }, []);
+  }, [n]);
   return {todos, loading};
 }
 
