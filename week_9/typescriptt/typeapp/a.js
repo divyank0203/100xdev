cdc2d6de385754d5fb1e6f658658d73769ceb80a
divyank0203 <divyankh444@gmail.com>
2025-10-23 17:55:56 +0530

    typescript syntax familiarization

---
+++ week_9/typescriptt/typeapp/a.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 44;
+console.log(x);
+const y = "Helloooo";
+console.log(y);
+//# sourceMappingURL=a.js.map
--- week_9/typescriptt/typeapp/a.ts
+++ week_9/typescriptt/typeapp/a.ts
@@ -1,3 +1,33 @@
 let x: number = 44;
 console.log(x);
-x="hello";
+
+const y: string = "Helloooo";
+console.log(y);
+
+function hello(name: string){
+    console.log(`Hello ${name}`);
+}
+
+hello("My name is this"); 
+
+function sum(a: number, b: number): number{
+    console.log("The Sum is: ") 
+    return a+b;
+}
+
+console.log(sum(4,5));
+
+function islegal(age: number ): boolean{
+    if(age>=18){
+        return true;
+    }
+    else return false;
+}
+
+console.log(islegal(20));
+
+function delay(fn: () => void, ms: number){
+    setTimeout(fn, ms);
+}
+
+delay(function(){console.log("Delayed hello")}, 2000);
